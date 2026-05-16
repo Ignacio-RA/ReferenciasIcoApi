07f83d8d4095693c44d5d0222bc6dff050643c2f
Ignacio-RA <ruiz.a.ignacio.a@gmail.com>
2026-05-16 12:32:45 -0600

    Eliminacion de inicioController.js y inicio_router.js, archivos creados para verificar la creación de la API

--- controllers/inicioController.js
+++
@@ -1,9 +0,0 @@
-const inicio=(req,res)=>{
-    res.json({
-        msg: "Hola",
-        status:"200",
-        descripcion: "Bienvenido a la API de referencias de ICO"
-    })
-}
-
-export {inicio}
--- routes/inicio_router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {inicio} from "../controllers/inicioController.js"
-
-const router=express.Router();
-
-//Routing
-router.get('/inicio',inicio)
-
-export default router
